7e9855cbad2485da3c183a9ff484e4b0dc458bd2
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-05-26 18:22:00 +0600

    solving problem-9

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,42 +1,3 @@
--- CREATE TABLE rangers(
---     ranger_id SERIAL PRIMARY KEY,
---     "name" VARCHAR(50) NOT NULL,
---     region VARCHAR(50) NOT NULL
--- );
-
--- INSERT INTO rangers ("name",region) VALUES
--- ('Alice Green', 'Northern Hills'),
--- ('Bob White', 'River Delta'),
--- ('Carol King', 'Mountain Range');
-
--- CREATE TABLE species(
---     u SERIAL PRIMARY KEY,
---     common_name VARCHAR(75) NOT NULL,
---     scientific_name VARCHAR(100) NOT NULL,
---     discovery_date DATE NOT NULL,
---     conservation_status VARCHAR(150) NOT NULL
--- );
-
--- INSERT INTO species (common_name,scientific_name,discovery_date,conservation_status) VALUES
--- ('Snow Leopard    ', 'Panthera uncia','1775-01-01','Endangered'),
--- ('Bengal Tiger    ', 'Panthera tigris tigris' ,'1758-01-01','Endangered'),
--- ('Red Panda       ', 'Ailurus fulgens','1825-01-01','Vulnerable'),
--- ('Asiatic Elephant', 'Elephas maximus indicus','1758-01-01','Endangered');
-
--- CREATE TABLE sightings(
---     sighting_id  SERIAL PRIMARY KEY,
---     8 INT REFERENCES species(species_id),
---     ranger_id  INT REFERENCES rangers(ranger_id ),
---     "location" VARCHAR(100) NOT NULL,
---     sighting_time TIMESTAMP NOT NULL,
---     notes TEXT
--- );
--- INSERT INTO sightings(species_id,ranger_id, "location",sighting_time,notes) VALUES
--- (1, 1, 'Peak Ridge'       , '2024-05-10 07:45:00', 'Camera trap image captured'),
--- (2, 2, 'Bankwood Area'    , '2024-05-12 16:20:00',' Juvenile seen'             ),
--- (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00',' Feeding observed'          ),
--- (1, 2, 'Snowfall Pass'    , '2024-05-18 18:30:00',NULL);
-
 --Problem 1
 INSERT INTO
     rangers ("name", region)
@@ -101,4 +62,13 @@ SELECT
     END AS time_of_day
 FROM sightings;
 
-SELECT * FROM rangers;
+--Problem 9
+DELETE FROM rangers
+WHERE
+    ranger_id IN (
+        SELECT ranger_id
+        FROM rangers
+            LEFT JOIN sightings USING (ranger_id)
+        WHERE
+            sighting_id IS NULL
+    );
